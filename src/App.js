dc5853accf06b85ffe54336238e3d157a2cdef57
Stefano Casasola <ste.casasola@gmail.com>
2024-03-18 11:02:28 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import MovieNavbar from './components/MovieNavbar'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    movieTitle: 'Iron man',
+  }
+
+  render() {
+    return (
+      <div>
+        <MovieNavbar />
+        <Container>
+          <Row className="justify-content-center">
+            <Col xs={12} md={8} lg={6}>
+              {/* ora vado ad inserire l'input field di tipo select per scegliere un film */}
+              {/* per ora lo inserirò qui senza crearne un componente separato, se ci avanza tempo lo facciamo dopo :) */}
+              <Form>
+                <Form.Group className="my-3">
+                  <Form.Label id="select-label">Scegli un film</Form.Label>
+                  <Form.Select
+                    onChange={(e) => {
+                      // cambio lo stato ogni volta che scelgo un nuovo film
+                      this.setState({
+                        movieTitle: e.target.value,
+                      })
+                    }}
+                    // seconda mandata: il valore di questa select sarà indissolubilmente collegato allo stato
+                    value={this.state.movieTitle}
+                  >
+                    <option>Iron man</option>
+                    <option>Dr. Strange</option>
+                    <option>Venom</option>
+                    <option>Thor: Love & Thunder</option>
+                    <option>Deadpool</option>
+                    <option>Guardiani della Galassia Vol.1</option>
+                  </Form.Select>
+                </Form.Group>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
